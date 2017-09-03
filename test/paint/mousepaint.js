7b67a0e8bd257bbc3d277e2c16fe70f8652ca78c
1take <ichikawa1014@gmail.com>
2017-09-03 23:54:49 +0900

    change remote color blue: add miss

--- test/paint/mousepaint.js
+++ test/paint/mousepaint.js
@@ -17,7 +17,7 @@ function writeByMsg(msg) {
         var endX = points[i], endY = points[i + 1];
         ctx.moveTo(startX, startY);
         ctx.lineTo(endX, endY);
-        ctx.strokeStyle = "red";
+        ctx.strokeStyle = "blue";
         ctx.stroke();
     }
 }
