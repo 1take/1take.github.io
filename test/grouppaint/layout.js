6d4b4f868d705c4fd76cfede9a279413c49929cc
1take <ichikawa1014@gmail.com>
2017-09-18 23:46:04 +0900

    add page (go/back) button

--- test/grouppaint/layout.js
+++ test/grouppaint/layout.js
@@ -9,12 +9,12 @@ $(function() {
   function sizing() {
     if (container.offsetHeight > container.offsetWidth * 4.0 / 3.0) {
       canvas.height = container.offsetWidth * 4.0 / 3.0;
+      container.height = container.offsetWidth * 4.0 / 3.0;
       canvas.width = container.offsetWidth;
     } else {
       canvas.height = container.offsetHeight;
       canvas.width = container.offsetHeight * 3.0 / 4.0;
     }
-
 
     //var pencil = $("#pencil");
     //document.getElementById('pencil').width = 200; //container.offsetWidth / action_button_num;
@@ -23,6 +23,33 @@ $(function() {
     $(".action_button").css("margin", action.offsetWidth * 0.8 / action_button_num / 25.0);
 
   }
+
+  var go = document.getElementById('go');
+  go.addEventListener('click', function () {
+
+      if (document.getElementById('mycanvas2') === null ) {
+        var canvas2 = document.createElement('canvas');
+        canvas2.id = 'mycanvas2';
+        canvas2.width = canvas.width;
+        canvas2.height = canvas.height;
+        canvas2.setAttribute('class', 'canvas');
+        container.appendChild(canvas2);
+
+        addPaintingListener('mycanvas2')
+      }
+
+      $("#mycanvas").css("display", "none");
+      $("#mycanvas2").css("display", "");
+    }
+  );
+
+  var back = document.getElementById('back');
+  back.addEventListener('click',
+    function () {
+      $("#mycanvas").css("display", "");
+      $("#mycanvas2").css("display", "none");
+      addPaintingListener('mycanvas2')    
+  });  
 
   window.addEventListener('resize', function() {
     (!window.requestAnimationFrame) ? setTimeout(sizing, 300): window.requestAnimationFrame(sizing);  
--- test/grouppaint/mousepaint.js
+++ test/grouppaint/mousepaint.js
@@ -34,112 +34,96 @@ function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+
+function addPaintingListener(mycanvas) {
+    var canvas = document.getElementById(mycanvas);
+    if (!canvas || !canvas.getContext) {
+        return false;
+    }
+    var ctx = canvas.getContext('2d');
+    //マウスの座標を取得
+    var mouse = {
+        x: 0,
+        y: 0,
+        color: "black",
+        isDrawing: false
+    };
+    var borderWidth = 1;
+    function move(e) {
+        //2.マウスが動いたら座標値を取得
+        var rect = e.target.getBoundingClientRect();
+        var cx, cy, ev;
+        if (e.type.indexOf('touch') != -1)
+            ev = e.touches[0];
+        else
+            ev = e;
+        mouse.x = ev.clientX - rect.left - borderWidth;
+        mouse.y = ev.clientY - rect.top - borderWidth;
+        //4.isDrawがtrueのとき描画
+        if (mouse.isDrawing) {
+            mouseStroke.push(mouse.x);
+            mouseStroke.push(mouse.y);
+            if (mouseStroke.length == 0) return;
+            var max = mouseStroke.length - 1; 
+            var curX = mouseStroke[max - 1], curY = mouseStroke[max];
+            var prevX = mouseStroke[max - 3], prevY = mouseStroke[max - 2];
+            ctx.beginPath();
+            ctx.moveTo(prevX, prevY);
+            ctx.lineTo(curX, curY);
+            ctx.strokeStyle = mouse.color;
+            ctx.stroke();
+        }
+    };
+    function start(e) {
+        mouseStroke = [];
+        mouse.isDrawing = true;
+    };
+    function end(e) {
+        mouse.isDrawing = false;
+        if (mouseStroke.length > 3) {
+            var msg = mouseStroke.join(" ");
+            var canvas = document.getElementById(mycanvas);
+            msg = canvas.width + " " + canvas.height + " " + msg;
+            sendMsg(msg);
+        }
+    };
+    function cancel(e) {
+        end(e);
+    };
+    function prevent(f, e) {
+        e.preventDefault();
+        f(e);
+    }
+    canvas.addEventListener("mousemove", function (e) {
+        prevent(move, e);
+    });
+    canvas.addEventListener("touchmove", function (e) {
+        $('#messages').append('<br>Event:<br>' + "move");
+        prevent(move, e);
+    });
+    canvas.addEventListener("mousedown", function (e) {
+        prevent(start, e);
+    });
+    canvas.addEventListener("touchstart", function (e) {
+        $('#messages').append('<br>Event:<br>' + "start");
+        prevent(start, e);
+    });
+    canvas.addEventListener("mouseup", function (e) {
+        prevent(end, e);
+    });
+    canvas.addEventListener("touchend", function (e) {
+        prevent(end, e);
+    });
+    canvas.addEventListener('mouseleave', function (e) {
+        prevent(cancel, e);
+    });
+    canvas.addEventListener('touchcancel', function (e) {
+        prevent(cancel, e);
+    });
+}
+
 (function () {
     $(document).ready(function () {
-        var canvas = document.getElementById('mycanvas');
-        if (!canvas || !canvas.getContext) {
-            return false;
-        }
-        var ctx = canvas.getContext('2d');
-
-        //マウスの座標を取得
-        var mouse = {
-            x: 0,
-            y: 0,
-            color: "black",
-            isDrawing: false
-        };
-        var borderWidth = 1;
-
-        function move(e) {
-            //2.マウスが動いたら座標値を取得
-            var rect = e.target.getBoundingClientRect();
-
-            var cx, cy, ev;
-
-            if (e.type.indexOf('touch') != -1)
-                ev = e.touches[0];
-            else
-                ev = e;
-
-            mouse.x = ev.clientX - rect.left - borderWidth;
-            mouse.y = ev.clientY - rect.top - borderWidth;
-
-            //4.isDrawがtrueのとき描画
-            if (mouse.isDrawing) {
-
-                mouseStroke.push(mouse.x);
-                mouseStroke.push(mouse.y);
-
-                if (mouseStroke.length == 0) return;
-
-                var max = mouseStroke.length - 1; 
-                var curX = mouseStroke[max - 1], curY = mouseStroke[max];
-                var prevX = mouseStroke[max - 3], prevY = mouseStroke[max - 2];
- 
-                ctx.beginPath();
-                ctx.moveTo(prevX, prevY);
-                ctx.lineTo(curX, curY);
-                ctx.strokeStyle = mouse.color;
-                ctx.stroke();
-            }
-        };
-
-        function start(e) {
-            mouseStroke = [];
-
-            mouse.isDrawing = true;
-        };
-
-        function end(e) {
-            mouse.isDrawing = false;
-
-            if (mouseStroke.length > 3) {
-                var msg = mouseStroke.join(" ");
-                var canvas = document.getElementById('mycanvas');
-                msg = canvas.width + " " + canvas.height + " " + msg;
-                sendMsg(msg);
-            }
-        };
-
-        function cancel(e) {
-            end(e);
-        };
-
-        function prevent(f, e) {
-            e.preventDefault();
-            f(e);
-        }
-
-        canvas.addEventListener("mousemove", function (e) {
-            prevent(move, e);
-        });
-        canvas.addEventListener("touchmove", function (e) {
-            $('#messages').append('<br>Event:<br>' + "move");
-            prevent(move, e);
-        });
-
-        canvas.addEventListener("mousedown", function (e) {
-            prevent(start, e);
-        });
-        canvas.addEventListener("touchstart", function (e) {
-            $('#messages').append('<br>Event:<br>' + "start");
-            prevent(start, e);
-        });
-
-        canvas.addEventListener("mouseup", function (e) {
-            prevent(end, e);
-        });
-        canvas.addEventListener("touchend", function (e) {
-            prevent(end, e);
-        });
-
-        canvas.addEventListener('mouseleave', function (e) {
-            prevent(cancel, e);
-        });
-        canvas.addEventListener('touchcancel', function (e) {
-            prevent(cancel, e);
-        });
-
+      addPaintingListener('mycanvas');
     });
 })();
